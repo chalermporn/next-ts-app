9ca44143fa68b8ce957f92730ffef27a0fcd1570
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-08-27 21:30:29 +0700

    feat: optimized ssr config test

--- next.config.js
+++ next.config.js
@@ -5,6 +5,31 @@ const nextConfig = {
   images: {
     domains: ['tailwindui.com'],
   },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+  devIndicators: {
+    autoPrerender: true,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
+  exportPathMap: async function ( defaultPathMap, { dev, dir, outDir, distDir, buildId } ) 
+  {
+    return {
+      '/': { page: '/' },
+      '/404': { page: '/404' },
+      '/api/hello': { page: '/api/hello', query: { title: 'hello-nextjs' } },
+    }
+  },
 }
 
 module.exports = nextConfig
